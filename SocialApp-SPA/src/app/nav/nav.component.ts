a8a4b84311c2601ef2a5a448c931f9eec2c7d4d3
Curtis Vermeeren <curtis.vermeeren@gmail.com>
2020-04-23 20:09:36 -0400

    Complete module 7

--- SocialApp-SPA/src/app/nav/nav.component.ts
+++ SocialApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Route } from '@angular/compiler/src/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-nav',
@@ -12,7 +14,7 @@ export class NavComponent implements OnInit {
         
     };
 
-    constructor(public authService: AuthService, private alertify: AlertifyService) { }
+    constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
     ngOnInit() {
     }
@@ -20,9 +22,11 @@ export class NavComponent implements OnInit {
     // login calls the authorization 
     login() {
         this.authService.login(this.model).subscribe(next => {
-            this.alertify.success('Logged in successfully')
+            this.alertify.success('Logged in successfully');
         }, error => {
-            this.alertify.error(error)
+            this.alertify.error(error);
+        }, () => {
+            this.router.navigate(['/members']);
         });
     }
 
@@ -35,6 +39,7 @@ export class NavComponent implements OnInit {
     logout() {
         localStorage.removeItem('token');
         this.alertify.message('Logged out');
+        this.router.navigate(['/home'])
     }
 
     
