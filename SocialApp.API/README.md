593e62d35f8127c092f949101c21cd4a0aa34a63
Curtis Vermeeren <curtis.vermeeren@gmail.com>
2020-06-29 20:35:45 -0400

    Section 8 Extending the API

--- SocialApp.API/README.md
+++ SocialApp.API/README.md
@@ -4,7 +4,7 @@ The <UserSecretsId> section of <Property Group> defines which secrets are stored
 
 The `AppSettings:Token` is used by jwt to give a secret key for signing tokens. 
 
-The `ConnectionStrings:DefaultConnection` is used as a connection string to the database. 
+The `ConnectionStrings:DefaultConnection` is used as a connection string to the database. Using a Sql Lite database
 
 ## Setup Secrets
 Add a unique <UserSecretsId> to the <Property Group> tag in the .csproj file.
@@ -12,3 +12,8 @@ Add a unique <UserSecretsId> to the <Property Group> tag in the .csproj file.
 Edit the `SECRETS.TEMPLATE.json` file to configure your environment. 
 
 Use the command `type .\SECRETS.TEMPLATE.json | dotnet user-secrets set` to set the secrets from the template file
+
+## Other
+Users seeded using json-generator.com and in the `Data/UserSeedData.json` file.
+
+Uncomment `seeder.SeedUsers();` in the `Congfigure` method of `Startup.cs` to seed the database with fake users. 
--- SocialApp.API/Startup.cs
+++ SocialApp.API/Startup.cs
@@ -19,6 +19,7 @@ using System.Net;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using SocialApp.API.Helpers;
+using AutoMapper;
 
 namespace SocialApp.API
 {
@@ -35,9 +36,15 @@ namespace SocialApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(opt => {
+                    opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
+            services.AddTransient<Seed>();
             services.AddCors();
+            services.AddAutoMapper();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<ISocialRepository, SocialRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters{
@@ -50,7 +57,7 @@ namespace SocialApp.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -73,6 +80,7 @@ namespace SocialApp.API
             }
             
             //app.UseHttpsRedirection();
+            //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
