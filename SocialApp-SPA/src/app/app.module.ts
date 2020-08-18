b21a60df7a39a6bb59bdf2654cb4093463ac7730
Curtis Vermeeren <curtis.vermeeren@gmail.com>
2020-08-18 16:17:18 -0400

    Section 10

--- SocialApp-SPA/src/app/app.module.ts
+++ SocialApp-SPA/src/app/app.module.ts
@@ -24,6 +24,9 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export function tokenGetter() {
     return localStorage.getItem('token');
@@ -40,6 +43,7 @@ export function tokenGetter() {
       MessagesComponent,
       MemberCardComponent,
       MemberDetailComponent,
+      MemberEditComponent,
    ],
    imports: [
       BrowserModule,
@@ -62,9 +66,11 @@ export function tokenGetter() {
       ErrorInterceptorProvider,
       AlertifyService,
       AuthGuard,
+      PreventUnsavedChanges,
       UserService,
       MemberDetailResolver,
       MemberListResolver,
+      MemberEditResolver,
    ],
    bootstrap: [
       AppComponent
--- SocialApp.API/Helpers/AutoMapperProfiles.cs
+++ SocialApp.API/Helpers/AutoMapperProfiles.cs
@@ -24,6 +24,7 @@ namespace SocialApp.API.Helpers
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
