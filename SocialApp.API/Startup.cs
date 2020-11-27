f27fb53215b23c6df2ca25d1490b11c8ec816b0b
Curtis Vermeeren <curtis.vermeeren@gmail.com>
2020-11-26 21:49:11 -0500

    Section 17
    Final Part

--- SocialApp.API/Startup.cs
+++ SocialApp.API/Startup.cs
@@ -85,7 +85,14 @@ namespace SocialApp.API
             //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Fallback", action = "Index"}
+                );
+            });
         }
     }
 }
